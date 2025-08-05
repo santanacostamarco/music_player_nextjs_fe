36d49a4428408e4cb4720ee2e3ad3900e91b418d
Marco Antonio Costa Santana <marcosantana@cvccorp.com.br>
2025-08-05 13:27:52 -0300

    chore: next config build images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,17 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['i.scdn.co', 'spotifycdn.com'],
+    domains: ['i.scdn.co', 'spotifycdn.com', 'mosaic.scdn.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.scdn.co*',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.spotifycdn.com*',
+      },
+    ],
   },
 };
 
